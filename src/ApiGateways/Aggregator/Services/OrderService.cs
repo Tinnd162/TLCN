05da3217e38da9d49a6578deeb118f222192bea7
VietLH <lamhv155@gmail.com>
2021-12-05 23:48:47 +0700

    update api gộp

--- src/ApiGateways/Aggregator/Services/OrderService.cs
+++ src/ApiGateways/Aggregator/Services/OrderService.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace Aggregator.Services
 {
-    public class OrderService
+    public class OrderService : IOrderService
     {
+        private readonly HttpClient _client;
+        public OrderService(HttpClient client)
+        {
+            _client = client;
+        }
+        public async Task<string> InsertSaleOrder(object objRequest)
+        {
+            var myContent = JsonConvert.SerializeObject(objRequest);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var response = _client.PostAsync($"/Order/Insert", byteContent).Result;
+            return await response.ReadContentAs<string>();
+        }
     }
 }
